d6cc12550944e06c938ea3a34e6584472c8a94db
Danushka <akhsunahd@gmail.com>
2016-10-16 22:04:25 +0530

    all php files added

---
+++ Contact.php
@@ -0,0 +1,374 @@
+<!DOCTYPE html>
+<html lang="en"><head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="images/jdr-logo.png">
+    <title>JEDARA Imports and Exports </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="keywords">
+    <meta name="author" content="">
+    
+
+
+    <link href="css/main.css" rel="stylesheet">
+
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+    
+    <link href="css/widgets.css" rel="stylesheet">
+
+    
+
+    <!--Link to google plus-->
+    <link href="https://plus.google.com/111575435043682440602" rel="publisher">
+    <div class="fit-vids-style">­<style>               .fluid-width-video-wrapper {                 width: 100%;                              position: relative;                       padding: 0;                            }                                                                                   .fluid-width-video-wrapper iframe,        .fluid-width-video-wrapper object,        .fluid-width-video-wrapper embed {           position: absolute;                       top: 0;                                   left: 0;                                  width: 100%;                              height: 100%;                          }                                       </style></div><script src="Self%20Manage%20Rental%20Property%20-%20Contact_files/track.asc" defer="defer" async="" type="application/javascript"></script><script async="" src="Self%20Manage%20Rental%20Property%20-%20Contact_files/cbgapi.loaded_1"></script><script src="Self%20Manage%20Rental%20Property%20-%20Contact_files/sdk.js" id="facebook-jssdk"></script><script gapi_processed="true" src="Self%20Manage%20Rental%20Property%20-%20Contact_files/platform.js" async="" type="text/javascript"></script><script src="Self%20Manage%20Rental%20Property%20-%20Contact_files/X1ge67e2aIg2fzoxanu4g.js" async="" type="text/javascript"></script><script async="" src="Self%20Manage%20Rental%20Property%20-%20Contact_files/cbgapi.loaded_0"></script><script src="Self%20Manage%20Rental%20Property%20-%20Contact_files/analytics.js" async=""></script><script gapi_processed="true" src="Self%20Manage%20Rental%20Property%20-%20Contact_files/plusone.js" async="" type="text/javascript"></script><script type="text/javascript">
+
+        (function () {
+            var po = document.createElement("script"); po.type = "text/javascript"; po.async = true;
+            po.src = "https://apis.google.com/js/plusone.js?publisherid=111575435043682440602";
+            var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(po, s);
+          
+        })();
+
+    </script>
+    <!--Facebook Open Graph metatags begins-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <meta property="og:title">
+    <meta property="og:description">
+    <meta property="og:site_name" content="Rentpluz">       
+    <meta property="og:url">
+    <meta property="og:image">
+
+    <!--Facebook Open Graph metatags ends-->
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date(); a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-51668624-1', 'rentpluz.com');
+        ga('send', 'pageview');
+
+    </script>
+</head>
+<body>
+    
+    <div class="navbar-wrapper">
+        <div class="navbar navbar-fixed-top" id="navigation">
+            <div class="navbar-inner">
+                <div class="container">
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="brand" >
+                        <img src="images/jdr-logo.png" >
+                    </a>
+                    <input id="navActive" type="hidden">
+                    <div class="nav-collapse collapse">
+                        
+                         
+      <div class="row" style="padding-top:15px">
+
+
+<span style="float:left">
+<h3>  JEDARA IMPORTS & EXPORTS (PVT) LTD </h3>
+   </span>
+
+<ul class="nav pull-right">
+    <li id="navHome" class="">
+      <a href="index.html">HOME</a>
+    </li>
+   
+
+    
+
+    <li id="navPricing" class="">
+        <a href="Products.html">PRODUCTS</a>
+    </li>
+
+   
+    <li id="navContact" class="">
+        <a href="Contact.html">CONTACT</a>
+    </li>
+   
+</ul>
+
+
+</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+    <div id="wrap">
+        <div>
+            
+
+<div class="container">
+
+    <section id="contact">
+        <div class="row-fluid">
+            
+
+            <div class="span12">
+                <h2 class="icon-envelope-alt">
+                    Contact Us
+                    <img src="images/p2.png" >
+                </h2>
+      
+
+<div style="padding-top: 30px">
+              <ul class="unstyled" >
+                   
+<li class="fa-lg" style="line-height: 35px"><i class="fa fa-map-marker "></i>  Address : No.65A , Peiris Mawatha, Kalubowila, Dehiwala, Sri Lanka </li>
+<li class="fa-lg" style="line-height: 35px"><i class="fa fa-envelope-o"></i>  E-mail : jedaraimpex@gmail.com </li>
+<li class="fa-lg" style="line-height: 35px"><i class="  fa fa-envelope"></i>  Fax : +94114411957 </li>
+
+                    </ul>
+                   
+                 
+
+<div>
+
+
+
+
+<div style="padding-top: 30px">
+
+
+
+                <p class="subtext fa-lg" style="line-height: 20px; padding-left:1px">Send us your E-mail. So that we can contact you. </p>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div style="padding-top: 10px">
+            
+            <form id="form" target="_self"  action="">
+                
+<div class="row">
+
+                <div class="uvField uvField-email uvField-first span6" style="padding-left:20px">
+
+        <label class="uvFieldInner" for="email_2436">
+          <span class="uvFieldPlaceholder">Your email address</span>
+          <input class="uvFieldText" name="email" id="feed" title="Your email address" type="email">
+        </label>
+     
+
+      
+      </div>         
+<div class="span5">
+<div id="submit_status">
+                <p class="subtext fa-lg"> Submitted </p>
+           </div>
+ 
+</div>
+
+       </div>
+
+      </div>
+                
+                <div style="width: 100%; display: block; float: right;">
+                     <button type="submit" class="uvStyle-button">Send</button>
+                </div>
+
+            </form>
+
+       </div>
+        <script type="text/javascript">
+            function postToGoogle() {
+              //  var field1 = $("input[type='radio'][name='qs1']:checked").val();
+               // var field2 = $("input[type='radio'][name='qs2']:checked").val();
+                var field3 = $('#feed').val();
+ 
+                $.ajax({
+                    url: "https://script.google.com/macros/s/AKfycbyfVnPFfjgomsdlloJ_oirkrT70cAJSV1SP7J-Dr9NOAT5I-n0/exec",
+                    data: {"name": field3},
+                    type: "POST",
+                    statusCode: {
+                        0: function() {
+                            alert("Error");
+                        },
+                        200: function() {
+                           // alert("success");   //Success Message
+                        $('#submit_status').show();
+                         $('#submit_status').hide().fadeOut("slow");
+
+                        }
+                    }
+                });
+/*                $.ajax({
+                    url: "https://docs.google.com/forms/d/1sLNZ_L5jpB5sLO-Q9jdkXbl7bhULcZtHtnmyOy3L_dM/formResponse",
+                    data: {"entry.1884265043": field3,
+                        "entry.513669972": field3,
+                        "entry.1212348438": field3,
+                        "fvv": 1,
+                        "draftResponse": "[,,\"2487897726520567894\"]",
+                        "pageHistory": 0,
+                        "fbzx": "2487897726520567894"
+                    },
+                    type: "POST",
+                    dataType: "application/xml",
+                    statusCode: {
+                        0: function() {
+                            alert("Error");
+                        },
+                        200: function() {
+                         
+                           alert("success");   //Success Message
+                        }
+                    }
+                });
+*/            }
+             
+            $(document).ready(function(){
+
+                      $('#submit_status').hide();
+
+
+                $('#form').submit(function() {
+                    postToGoogle();
+                    return false;
+                });
+            });
+        </script>
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+                <div class="uservoice-container">
+                    
+                    <script>
+                        (function () {
+                            var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true; uv.src = ('https:' === document.location.protocol ? 'https:' : 'http:') + '//widget.uservoice.com/X1ge67e2aIg2fzoxanu4g.js'; var s = document.getElementsByTagName('script')[0];
+                            s.parentNode.insertBefore(uv, s);
+                        })()
+                    </script>
+
+                    
+
+                    <div style="position: relative;" data-uv-scanned="true" data-uv-inline="classic_widget" data-uv-mode="full" data-uv-primary-color="#25395c" data-uv-link-color="#007dbf" data-uv-default-mode="support" data-uv-forum-id="247810" data-uv-width="100%" data-uv-height="550px"><iframe src="Self%20Manage%20Rental%20Property%20-%20Contact_files/classic_widget.html" allowtransparency="true" style="display: block; background: transparent none repeat scroll 0% 0%; border: medium none; width: 100%; height: 550px; visibility: visible;" class="uvw-dialog-iframe" name="uvw-iframe-undefined" frameborder="0"></iframe></div>
+                </div>
+
+
+
+
+                _-->
+                
+            </div>
+        </div>
+<!--
+        <div class="row-fluid">
+            <div class="span12">
+                <h2 class="icon-map-marker">Find Us<img src="Self%20Manage%20Rental%20Property%20-%20Contact_files/p3.png" alt="www.rentpluz.com"></h2>
+                <p class="subtext">We are here!</p>
+                <div>
+                    <iframe scrolling="no" marginheight="0" marginwidth="0" src="Self%20Manage%20Rental%20Property%20-%20Contact_files/maps.html" frameborder="0" height="350" width="425"></iframe>
+                    <br>
+                    <small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=82+wentworth+avenue,+kingston+ACT+2604&amp;aq=&amp;sll=-35.320203,149.146861&amp;sspn=0.0078,0.013937&amp;ie=UTF8&amp;hq=&amp;hnear=82+Wentworth+Ave,+Kingston+Australian+Capital+Territory+2604,+Australia&amp;t=m&amp;z=14&amp;ll=-35.320203,149.146864" style="color: #0000FF; text-align: left">View Larger Map</a></small>
+                </div>
+            </div>
+        </div>
+    -->
+    </section>
+
+</div>
+
+
+        </div>
+        
+        
+    </div><div></div>
+    
+    <div>
+ <footer id="footer" class="margintop20">
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+             
+                <ul class="unstyled">
+                
+                
+                    
+                    <li> <a href="https://www.jedara.com/Contact">Contact Us</a> </li>
+    
+                </ul>
+
+            </div>
+          
+            
+
+            
+
+
+        </div>
+    </div>
+
+</footer>
+
+    </div>
+    
+    
+    
+
+
+
+    
+    
+
+
+
+
+</body></html>
