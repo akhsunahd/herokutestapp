b3e8e64f7eef3de997cd7d70fdd3c4dbc076a3ea
danushka <danushka.madusanka@avtra.com>
2020-01-26 14:49:21 +0100

    d connect

--- location.php
+++ location.php
@@ -1,6 +1,15 @@
 <?php
 	//error_log( $_GET['val'], 3, "C:\\xampp\\tmp\\my-errors.log");
+   $host        = "host = ec2-3-224-165-85.compute-1.amazonaws.com";
+   $port        = "port = 5432";
+   $dbname      = "dbname = dd2g7aamsbmv73";
+   $credentials = "user = natsflgfpoqmlr password=129a5ddf67b9a93cf5e75afcd179e48ea1a1e890a82f9267447b72a565751022";
 
-echo "data recived";
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 
 ?>
